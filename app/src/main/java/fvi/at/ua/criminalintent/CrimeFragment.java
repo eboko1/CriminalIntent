8b5190e3e705ff32b67c786417b3d3988dc9682f
Victoria Frantsuh <eboko1@gmail.com>
2017-05-19 23:47:30 +0300

    add new TextWatcher()

--- app/src/main/java/fvi/at/ua/criminalintent/CrimeFragment.java
+++ app/src/main/java/fvi/at/ua/criminalintent/CrimeFragment.java
@@ -3,10 +3,12 @@ package fvi.at.ua.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.LayoutInflaterCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -15,6 +17,8 @@ import android.view.ViewGroup;
 
 public class CrimeFragment extends Fragment {
     private  Crime mCrime;
+    private EditText mTitleField;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState ){
@@ -24,6 +28,25 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_crime, container,false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setmTitle((s.toString()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         return v;
     }
 }
