cf267e44a3182a74ee98da43e0611da260a02157
Victoria Frantsuh <eboko1@gmail.com>
2017-05-19 23:55:23 +0300

    get Fragment manager

--- app/src/main/java/fvi/at/ua/criminalintent/CrimeActivity.java
+++ app/src/main/java/fvi/at/ua/criminalintent/CrimeActivity.java
@@ -1,8 +1,11 @@
 package fvi.at.ua.criminalintent;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -10,5 +13,7 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+     FragmentManager fm  = getSupportFragmentManager();
     }
 }
