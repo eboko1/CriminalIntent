015a146a9bde0e9f458c737d1ba969142417c7ad
Victoria Frantsuh <eboko1@gmail.com>
2017-05-21 15:06:00 +0300

    add List<Crime> in CrimeLab.class

--- app/src/main/java/fvi/at/ua/criminalintent/Crime.java
+++ app/src/main/java/fvi/at/ua/criminalintent/Crime.java
@@ -8,6 +8,7 @@ import java.util.UUID;
  */
 
 public class Crime {
+    
     private UUID mId;
     private String mTitle;
 
--- app/src/main/java/fvi/at/ua/criminalintent/CrimeLab.java
+++ app/src/main/java/fvi/at/ua/criminalintent/CrimeLab.java
@@ -2,6 +2,9 @@ package fvi.at.ua.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Vika on 21.05.2017.
  */
@@ -10,14 +13,18 @@ public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
 
-    private CrimeLab(Context context){
+    private List<Crime> mCrimes;
 
-    }
+
     public static CrimeLab get(Context context){
         if (sCrimeLab == null){
             sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
+    }
+
+    private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
     }
 
 }
